f488ba34a808ce166cf01610d991dfd9b2803a1d
Beelzebub3 <davinascimentojobs@gmail.com>
2024-08-14 17:04:27 -0300

    update

--- Trabalhos--TDS/ValidaçãoDeAlgoritmo/index.js
+++ Trabalhos--TDS/ValidaçãoDeAlgoritmo/index.js
@@ -1,20 +1,29 @@
 // Selecione um algoritmo de sua preferência entre(Verificar se um numero é par ou impar, converter Celcius para Fahrenheit) 
 
 
-// verificar se o numero e par ou impar
+
 const nomeDoUser = prompt("Digite seu nome: ")
+// verificar se o numero e par ou impar
+
+// O comando prompt naturalmente entrega a resposta do cliente como uma string. com o metodo Number convertemos essa string para number
 const numeroDoUser = Number(prompt("Digite um numero: "))
 
-/* Verificando se o resto da divisão do numero é igual a 0
+alert(typeof numeroDoUser)
+alert(numeroDoUser)
+
+
+/* Verificando se o resto da divisão do numero por 2 é igual a 0
 CASO o resto da divisão for igual a 0, o numero do user é par
 CASO contrario o numero é impar
 */
-if(numeroDoUser % 2 == 0) {
-  alert("o numero é par")
+
+if(numeroDoUser % 2 === 0) {
+    alert(`O número ${numeroDoUser} é par`)
 }
+
 else {
-    alert("O numero é impar")
-} 
+  alert(`O número ${numeroDoUser} é impar-`)
+}
 
 
 // Converter graus celcius em fahrenheit
@@ -26,6 +35,6 @@ Celsius → Fahrenheit
 Multiplicamos a temperatura em ºC por 1,8 e somamos 32 ao resultado.
 */
 
-alert(`A  temperatura do user: ${nomeDoUser} ==> { ${grausDoUser} graus celcius } convertida para fahrenheit é ${tempEmFahrenheit.toFixed(0)} fahrenheit`)
+alert(`A  temperatura do user: ${nomeDoUser} ==> { ${grausDoUser} celcius } convertida para fahrenheit é ${tempEmFahrenheit.toFixed(0)} fahrenheit`)
 
 
